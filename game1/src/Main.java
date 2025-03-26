5afdc90b03b3c50f58246334323991076ef20768
.email <sanjaisanjai6551@gmail.com>
2025-03-26 21:26:24 +0530

    upto boolen

--- game1/src/Main.java
+++ game1/src/Main.java
@@ -2,34 +2,89 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        System.out.println("Welcome to game");
+        boolean loop=true;
+        boolean key=false;
+        boolean code=false;
+        while(loop){
         System.out.println("You are stucked in  a room plx choose any optio below");
         System.out.println("1.check desk which has a drawer");
         System.out.println("2.check the misterious box");
         System.out.println("3.UNlock the door");
+            System.out.println("press 0 to exit");
+            int ex=-1;
+            if (ex==0){
+                System.exit(0);
+            }
         int result=in.nextInt();
-       boolean key=false;
-       boolean code=false;
+
+
+
        switch (result){
            case 1:
                System.out.println("you have pressed option 1");
                System.out.println("you found a note:'you have found a key' ");
+               System.out.println("  ");
+               System.out.println("  ");
+               System.out.println("  ");
                key=true;
                break;
                case 2:
                if (key==false){
                    System.out.println("the box is locked....Hmmmm something is missing");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
                }
                else {
                    System.out.println("you have unlocked door");
                    System.out.println("your passcode is 726523 ");
                    System.out.println("note decode the passcode on your phone keypad");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
                    code=true;
                }
-
-
-
-
-
+               break;
+           case 3:
+               if (key==false && code==false){
+                   System.out.println("the box is locked....Hmmmm something is missing");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
+               }
+               else if (key==true){
+                   System.out.println(" you have key but you need code to unlock");
+                   System.out.println("ooh shit ghost be like:enaku ithu pudikula");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
+               }
+               else if (code==true)
+               {
+                   System.out.println("you need key to ulock this door");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
+               }
+               else {
+                   System.out.println("you have unloced the room ");
+                   System.out.println("  ");
+                   System.out.println("  ");
+                   System.out.println("  ");
+               }
+               break;
+           case 4:
+               System.out.println("press 4 to exsit the game");
+               loop=false;
+               System.out.println("  ");
+               System.out.println("  ");
+               System.out.println("  ");
+           default:
+               System.out.println("invalid input ohhh wait the ghost arrived");
+               System.out.println("  ");
+               System.out.println("  ");
+               System.out.println("  ");
     }
-}}
+}
+}
+}
